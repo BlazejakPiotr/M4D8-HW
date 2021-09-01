69355fc7f43884a0410abc0f795ee00adaba755d
BlazejakPiotr <piotr.blazejak@yahoo.com>
2021-09-01 14:47:49 +0200

    Initial commit

---
+++ src/components/DisplayComments.jsx
@@ -0,0 +1,46 @@
+import { Component } from "react";
+
+class DisplayComments extends Component {
+  state = {
+    comments: [],
+  };
+
+  async componentDidMount() {
+    try {
+      const response = await fetch(
+        "https://striveschool-api.herokuapp.com/api/comments/" +
+          this.props.selectedMovie,
+        {
+          headers: {
+            Authorization:
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTFjZjYyZjJkNTI2MjAwMTViNmRjOTUiLCJpYXQiOjE2MjkyODc5ODMsImV4cCI6MTYzMDQ5NzU4M30.nNXVsU3Xm3rtBjwRiDZJUn8LcZawq-ItVaCq0cX5GZs",
+          },
+        }
+      );
+      const data = await response.json();
+      this.setState({
+        comments: data,
+      });
+    } catch {}
+  }
+
+  render() {
+    return (
+      <>
+        <h5>Comments:</h5>
+        <ul>
+          {this.state.comments.map((c) => (
+            <li>
+              {c.comment}
+              <i>
+                (Rate: {c.rate} / Date: {c.createdAt})
+              </i>
+            </li>
+          ))}
+        </ul>
+      </>
+    );
+  }
+}
+
+export default DisplayComments;
